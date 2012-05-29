544bf03ca7cd3c66a6f1ac4396c787dfadb56bde
Richard Klancer <rpk@pobox.com>
2012-05-29 16:30:58 -0400

    Script to plot extended KE time series

---
+++ models/epsilon-test/plot-extended-data.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def get_ke(line): 
+  return float(line.split()[2])
+
+def get_te(line):
+  return float(line.split()[3])
+
+initial_ke = map(lambda (x): float(x.split()[1]), open('index.txt').readlines()[4:])
+
+for i in (1, 5):
+  plt.subplot(2, 1, 1 + i/5, title="Next Gen MW Kinetic Energy, initial KE = {:.4f} eV".format(initial_ke[i-1]))
+  fname = 'nextgen/model{}-extended.data.txt'.format(i)
+  lines =  open(fname, 'r').readlines()
+  ke = map(get_ke, lines)
+  te = map(get_te, lines)
+  plt.ylim(2, 8)
+  plt.ylabel("Energy (eV)")
+  plt.plot(ke, label = "Kinetic Energy")
+  plt.plot(te, label = "Total Energy")
+  plt.legend()
+  
+plt.savefig('lj-extended-data', dpi=300)

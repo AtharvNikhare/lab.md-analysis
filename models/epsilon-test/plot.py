357e65f507ac7c8c484602e626fab1d24422207f
Richard Klancer <rpk@pobox.com>
2012-05-30 14:16:17 -0400

    Amend plotting script to include data from model 0 (0% KE)

--- models/epsilon-test/plot.py
+++ models/epsilon-test/plot.py
@@ -25,7 +25,7 @@ nextgen_control_sd = []
 
 print "Initial KE (eV)\tClassic KE (eV)\tsd\tNext Gen KE (eV)\tsd\tControl KE (eV)\tsd\n" 
 
-for i in range(1, 7):
+for i in range(0, 7):
   classic_fname = "classic/model{}.data.txt".format(i)
   nextgen_fname = "nextgen/model{}.data.txt".format(i)
   nextgen_control_fname = "nextgen/model{}-control.data.txt".format(i)
@@ -34,32 +34,34 @@ for i in range(1, 7):
   nextgen_data.append( map(get_nextgen_ke, open(nextgen_fname, 'r').readlines()) )
   nextgen_control_data.append( map(get_nextgen_ke, open(nextgen_control_fname, 'r').readlines()) )
 
-  classic_ke.append( np.mean(classic_data[i-1]) )
-  classic_sd.append( np.std(classic_data[i-1]) )
-  nextgen_ke.append( np.mean(nextgen_data[i-1]) )
-  nextgen_sd.append( np.std(nextgen_data[i-1]) )
-  nextgen_control_ke.append( np.mean(nextgen_control_data[i-1]) )
-  nextgen_control_sd.append( np.std(nextgen_control_data[i-1]) )
+  classic_ke.append( np.mean(classic_data[i]) )
+  classic_sd.append( np.std(classic_data[i]) )
+  nextgen_ke.append( np.mean(nextgen_data[i]) )
+  nextgen_sd.append( np.std(nextgen_data[i]) )
+  nextgen_control_ke.append( np.mean(nextgen_control_data[i]) )
+  nextgen_control_sd.append( np.std(nextgen_control_data[i]) )
 
   print "{:.4f}\t{:.4f}\t{:.4f}\t{:.4f}\t{:.4f}\t{:.4f}\t{:.4f}\t".format(
     initial_ke[i],
-    classic_ke[i-1],
-    classic_sd[i-1],
-    nextgen_ke[i-1],
-    nextgen_sd[i-1],
-    nextgen_control_ke[i-1],
-    nextgen_control_sd[i-1]
+    classic_ke[i],
+    classic_sd[i],
+    nextgen_ke[i],
+    nextgen_sd[i],
+    nextgen_control_ke[i],
+    nextgen_control_sd[i]
   )
 
 # actually plot stuff
 
-plt.errorbar(initial_ke[1:], classic_ke, yerr=classic_sd, label='Classic MW')
-plt.errorbar(initial_ke[1:], nextgen_ke, yerr=nextgen_sd, label='Next Gen MW')
-plt.errorbar(initial_ke[1:], nextgen_control_ke, yerr=nextgen_control_sd, label=r'Next Gen MW, incorrect $\epsilon$')
+plt.xlim(-0.5, 3.0)
+
+plt.errorbar(initial_ke, classic_ke, yerr=classic_sd, label='Classic MW')
+plt.errorbar(initial_ke, nextgen_ke, yerr=nextgen_sd, label='Next Gen MW')
+plt.errorbar(initial_ke, nextgen_control_ke, yerr=nextgen_control_sd, label=r'Next Gen MW, incorrect $\epsilon$')
 
 
 plt.title("KE at equilibrium vs. initial KE (total energy held constant)\nFor Next Gen MW as of 5-29-2012");
-plt.ylim(0, 7)
+plt.ylim(0, 8)
 plt.xlabel(r'Initial KE of model (eV)')
 plt.ylabel(r'Steady-state KE of model (eV)')
 plt.legend(loc='lower center')
@@ -70,9 +72,9 @@ plt.clf()
 
 
 def plot_one_series(data, name, row):
-  for i in range(1, 7):
-    plt.subplot(3, 6, i + (row - 1) * 6);
-    plt.ylim(0,7)
+  for i in range(0, 7):
+    plt.subplot(3, 7, i + (row - 1) * 7);
+    plt.ylim(3,8)
     plt.plot(data[i-1])
 
 plt.title("Time series of KE values")
